d82369cae458c8400f09a869f5b4508e70cc4043
Akash Verma <63600078+Akash2508@users.noreply.github.com>
2022-05-30 14:43:21 +0530

    set debug

--- resume/settings.py
+++ resume/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u%gw@u4*cn135yylml9=dq%$e_l*@mp#!53b5(1vz#cbbp(sq$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

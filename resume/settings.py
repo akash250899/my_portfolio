42cb7918949399839d776ec67a7f370c894fc499
Akash Verma <63600078+Akash2508@users.noreply.github.com>
2022-06-01 10:48:53 +0530

    added whitenoise

--- resume/settings.py
+++ resume/settings.py
@@ -137,6 +137,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_in_project')
 ]
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 

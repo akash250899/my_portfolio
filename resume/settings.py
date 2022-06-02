82386155074d097c1339d6fa25bf4bd91244b536
Akash Verma <63600078+Akash2508@users.noreply.github.com>
2022-06-02 11:58:57 +0530

    jchb

--- resume/settings.py
+++ resume/settings.py
@@ -132,32 +132,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static_in_project')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-# # STATICFILES_DIRS = [
-# #     os.path.join(BASE_DIR, 'staticfiles/')
-# # ]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static_in_project')
+]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static_in_project/')
 
 
-# Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
 
-
-# if DEBUG:
-#     STATICFILES_DIRS = [
-#         os.path.join(BASE_DIR, 'static')
-# ]
-# else:
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'static_in_project')
-
-
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 

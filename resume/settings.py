c2ca8bf0df0272eab77fc25834f53727a5e148ea
Akash Verma <63600078+Akash2508@users.noreply.github.com>
2022-06-02 11:37:47 +0530

    aa

--- resume/settings.py
+++ resume/settings.py
@@ -139,14 +139,14 @@ USE_TZ = True
 # # STATICFILES_DIRS = [
 # #     os.path.join(BASE_DIR, 'staticfiles/')
 # # ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_in_project')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_in_project')
+
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
 # if DEBUG:

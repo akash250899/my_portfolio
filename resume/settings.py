318e9f5f9762a5148e5902a076250711774a6eb3
Akash Verma <63600078+Akash2508@users.noreply.github.com>
2022-06-02 16:37:53 +0530

    f

--- resume/settings.py
+++ resume/settings.py
@@ -149,7 +149,7 @@ else:
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/resume/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
